de6ca5eb15f40ae64b8d836de05b5f8b69eb065b
Christiaan Quyn <christianq010@gmail.com>
2018-03-23 17:22:13 +0530

    Initial Commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{name}} </p>
+        <button v-on:click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            resetName() {
+                this.name = 'Max';
+                // custom event
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
